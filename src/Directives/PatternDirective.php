82448b2006990688ab36a380b3ba99e76ae5ac3f
Alex Vanderbist <alex.vanderbist@gmail.com>
2021-07-28 16:31:07 +0200

    Improve suggestions

--- src/Directives/PatternDirective.php
+++ src/Directives/PatternDirective.php
@@ -12,6 +12,17 @@ abstract class PatternDirective
 
     abstract public function pattern(): string;
 
+    /**
+     * The directives key will be used to differentiate between auto-completions
+     * for multiple of the same directives. E.g. 2 FuzzeKeyValueDirectives for 2 fields.
+     *
+     * @return string
+     */
+    public function getKey(): string
+    {
+        return static::class;
+    }
+
     public function transformToSuggestions(array $results): array
     {
         return [];
